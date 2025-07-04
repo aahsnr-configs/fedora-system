7163d1f6253f91fe3f14ea2b3fd60ebd5071c622
aahsnr <ahsanur041@proton.me>
2025-07-04 22:18:43 +0600

    updated

--- setup.sh
+++ setup.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 bash $HOME/fedora-setup/setup-scripts/01init
+bash $HOME/fedora-setup/setup-scripts/02fonts
+bash $HOME/fedora-setup/setup-scripts/03groups
+bash $HOME/fedora-setup/setup-scripts/04syspkgs
+bash $HOME/fedora-setup/setup-scripts/05security
+bash $HOME/fedora-setup/setup-scripts/06desktop
+bash $HOME/fedora-setup/setup-scripts/07git
